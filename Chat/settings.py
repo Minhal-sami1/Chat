a62db6a06170359fc94249c6508fa512a2c9688e
Minhal-sami1 <romeomino415@gmail.com>
2022-07-17 01:15:24 +0500

    works

--- Chat/settings.py
+++ Chat/settings.py
@@ -128,6 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
